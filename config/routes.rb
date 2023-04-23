281f554488653b6315e18547b08a558da709d475
Cobi Moos <cobimoos100@gmail.com>
2023-04-23 17:15:00 -0400

    added games controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :games, only: %i[index show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "games#index"
 end
