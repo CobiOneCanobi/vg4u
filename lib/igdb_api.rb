b845d315bc31759af8703ddc0d7e74f5627a53c7
Cobi Moos <cobimoos100@gmail.com>
2023-04-24 16:58:26 -0400

    Implemeting a method to grab game release_dates from IGDB

--- lib/igdb_api.rb
+++ lib/igdb_api.rb
@@ -1,16 +1,23 @@
 class IgdbApi
   include HTTParty
-  base_uri ENV['IGDB_URL']
+  base_uri 'https://api.igdb.com/v4'
 
-  #TODO add error handling for httparty requests
+  # TODO add error handling for httparty requests
   def initialize
     response = self.class.post("https://id.twitch.tv/oauth2/token?client_id=#{ENV['TWITCH_CLIENT_ID']}&client_secret=#{ENV['TWITCH_CLIENT_SECRET']}&grant_type=client_credentials
 ")
     access_token = response.parsed_response['access_token']
-    @header = { headers: {'Client-ID' => ENV['TWITCH_CLIENT_ID'], 'Authorization' => "Bearer #{access_token}"} }
+    @headers = {'Client-ID' => ENV['TWITCH_CLIENT_ID'], 'Authorization' => "Bearer #{access_token}"}
   end
 
-  def games
-    self.class.post('/games', @header)
+  def games(body = 'fields *;')
+    self.class.post('/games', headers: @headers, body: body)
+  end
+
+  def release_dates(body = 'fields game, date, platform;')
+    response = self.class.post('/release_dates', headers: @headers, body: body)
+    game_ids = response.map {|game| game['game'] }.join(',')
+    game_names = games("fields name; where id = (#{game_ids});")
+    # TODO move logic that is not related to grabbing release dates outside of method
   end
 end
