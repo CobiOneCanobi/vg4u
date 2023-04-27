6a2c06dc12814e74e91ecb5eecfcdc50c02f63ee
Cobi Moos <cobimoos100@gmail.com>
2023-04-27 16:05:22 -0400

    Implemeted a method to grab game cover art from IGDB

--- lib/igdb_api.rb
+++ lib/igdb_api.rb
@@ -26,15 +26,27 @@ class IgdbApi
     response = self.class.post('/release_dates', headers: @headers, body: body)
   end
 
+  # Sample output:
+  # [{"id"=>2473, "game"=>76870, "url"=> image url }]
+  # game: Reference id of Game
+  # url: url of cover image
+  def covers(body = 'fields *;')
+    response = self.class.post('/covers', headers: @headers, body: body)
+  end
+
   # TODO move into video game object
   def merging_video_game_data
     game_release_dates = release_dates
-    game_ids = game_release_dates.map {|release_date| release_date['game'] }.join(',')
+    game_ids = game_release_dates.map { |release_date| release_date['game'] }.join(',')
     games = games("fields name; where id = (#{game_ids});")
+    covers = covers("fields game, url; where game = (#{game_ids});")
+    no_cover_url = '//images.igdb.com/igdb/image/upload/t_thumb/nocover.jpg'
 
     game_release_dates.each do |release_date|
-      game = games.find {|game| game['id'] == release_date['game']}
-      release_date.merge!('name' => game['name'])
+      game = games.find { |game| game['id'] == release_date['game'] }
+      cover = covers.find { |cover| cover['game'] == release_date['game'] }
+      cover_url = cover.nil? ? no_cover_url : cover['url']
+      release_date.merge!('name' => game['name'], 'url' => cover_url)
     end
     game_release_dates
   end
