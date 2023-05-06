bc965f2219f425b78dd3c9c4c415f87203935dbe
Cobi Moos <cobimoos100@gmail.com>
2023-05-05 20:02:03 -0400

    modifying igdb results cover url and release date to human readable

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,11 +1,14 @@
 class Game
-  attr_accessor :id, :name, :cover_url, :release_date, :platform
+  # TODO review setter methods
+  attr_reader :id, :name, :cover_url, :release_date, :platform
+
+  NO_COVER_URL = 'https://images.igdb.com/igdb/image/upload/t_thumb/nocover.jpg'.freeze
 
   def initialize(id, options = {})
     @id = id
     @name = options[:name] || nil
-    @cover_url = options[:cover_url] || nil
-    @release_date = options[:release_date] || nil
+    @cover_url = options[:cover].nil? ? NO_COVER_URL : "https:#{options[:cover]['url']}"
+    @release_date = Time.at(options[:release_date]) || nil
     @platform = options[:platform] || nil
   end
 end
--- app/services/retrieve_games_service.rb
+++ app/services/retrieve_games_service.rb
@@ -6,16 +6,18 @@ class RetrieveGamesService
   def retrieve_games
     games = []
     game_release_dates = @igdb_api.release_dates
-    game_ids = game_release_dates.map { |release_date| release_date['game'] }.join(',')
+    game_ids = game_release_dates.map { |game_release_date| game_release_date['game'] }.join(',')
     game_names = @igdb_api.games("fields name; where id = (#{game_ids});")
-    covers = @igdb_api.covers("fields game, url; where game = (#{game_ids});")
-    no_cover_url = '//images.igdb.com/igdb/image/upload/t_thumb/nocover.jpg'
+    game_covers = @igdb_api.covers("fields game, url; where game = (#{game_ids});")
 
     game_release_dates.each do |game_release_date|
       game_name = game_names.find { |g| g['id'] == game_release_date['game'] }
-      cover = covers.find { |c| c['game'] == game_release_date['game'] }
-      cover_url = cover.nil? ? no_cover_url : cover['url']
-      game = Game.new(game_release_date['game'], name: game_name['name'], cover_url: cover_url, release_date: game_release_date['date'], platform: game_release_date['platform'] )
+      game_cover = game_covers.find { |c| c['game'] == game_release_date['game'] }
+      game = Game.new(game_release_date['game'],
+                      name: game_name['name'],
+                      cover: game_cover,
+                      release_date: game_release_date['date'],
+                      platform: game_release_date['platform'])
       games << game
     end
     games
